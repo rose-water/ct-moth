1804214ad1b351a43fbc0e54d7a1118cb1f75a6e
rose-water <rachel.ulgado@gmail.com>
2019-10-09 15:46:59 -0700

    moving test function, added rapid wing movement but need to map with light value

--- mothBehavior.py
+++ mothBehavior.py
@@ -4,7 +4,7 @@ import machine
 from servo import Servo
 
 # ------------------------------------------------------------
-# SETUP
+# PIN SETUP
 # ------------------------------------------------------------
 # Servo setup
 pin1 = machine.Pin(4)  # GPIO 4
@@ -14,15 +14,18 @@ wingLeft = Servo(pin1)
 wingRight = Servo(pin2)
 
 # Light sensor setup
-# A light sensor outputs values in the range 0 - 4096, where
-# 4096 is darkest and 0 is brightest. However in this circuit
-# it is reversed, with 0 indicating lowest level of light and 
-# higher numbers indicating higher levels of light. 
+# Outputs values in the range 0 - 4096, where 4096 is darkest, 0 
+# is brightest. However here it is reversed, with 0 indicating 
+# lowest level of light. 
 pin34 = machine.Pin(34) # A2
 adc = machine.ADC(pin34)
 adc.atten(machine.ADC.ATTN_11DB)
 
 lightVal = 0
+
+# MOTH FEELS!
+excitementLevel = 0
+hungerLevel = 0
 
 # ------------------------------------------------------------
 # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
@@ -62,29 +65,50 @@ def prepMoth():
 
 
 # ------------------------------------------------------------
+# The higher the level of light the more rapidly the moth's
+# wings should flap (meaning the delay should be less). 0.35 
+# seems to the the lowest amount we can do comfortably when 
+# going from 0 to 180.
+def flapBasedOnExcitement(lightLevel):
+    # delayAmt = translate(lightLevel)
+    wingLeft.write_angle(180)
+    wingRight.write_angle(0)
+    time.sleep(0.35)
+
+    wingLeft.write_angle(0)
+    wingRight.write_angle(180)
+    time.sleep(0.35)
+
+# ------------------------------------------------------------
+# A test function, opens wings (moves servos) in proportion to
+# level of light being read by the sensor
+def spreadWingsByLight(lightLevel):
+    wingAngle = translate(lightLevel, 3300, 4096, 0, 180)
+
+    if wingAngle < 0:
+        wingAngle = 0
+    
+    elif wingAngle > 180:
+        wingAngle = 180
+
+    wingLeft.write_angle(180 - wingAngle)
+    wingRight.write_angle(wingAngle)
+    
+    print("rightWingAngle: " + str(wingAngle) + ", leftWingAngle: " + str(180 - wingAngle))
+    
+    time.sleep(0.25)
+
+
+# ------------------------------------------------------------
 def runMoth():
     print("Initializing moth...")
-    
     prepMoth()
-    
-    while True: 
-        lightVal = adc.read()
-        print("lightVal: " + str(lightVal))
-        
-        wingAngle = translate(lightVal, 3000, 4096, 0, 180)
 
-        if wingAngle < 0:
-            wingAngle = 0
-        
-        elif wingAngle > 180:
-            wingAngle = 180
-            
-        wingLeft.write_angle(180 - wingAngle)
-        wingRight.write_angle(wingAngle)
-        
-        print("rightWingAngle: " + str(wingAngle) + ", leftWingAngle: " + str(180 - wingAngle))
-        
-        time.sleep(0.25)
+    while True:
+        lightVal = adc.read()
+        # spreadWingsByLight(lightVal)
+        flapBasedOnExcitement(lightVal)
+    
 
 # ------------------------------------------------------------
 runMoth()
